092fedb7314ce7585583a61248e0b7296e98a015
Joni Halabi <jh1955@georgetown.edu>
2023-07-18 16:18:42 -0400

    updating build scripts and jquery; updating list of links

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,33 @@
  * Build tasks.
  */
 
-const gulp      = require( 'gulp' );
-const sass      = require( 'gulp-sass' );
-const minifyCSS = require( 'gulp-minify-css' );
+const gulp       = require( 'gulp' ),
+      browserify = require( 'browserify' ),
+      buffer     = require( 'vinyl-buffer' ),
+      sass       = require( 'gulp-dart-sass' ),
+      source     = require( 'vinyl-source-stream' ),
+      uglify     = require( 'gulp-uglify' );
 
-gulp.task( 'default', function() {
+
+// SCSS.
+gulp.task( 'scss', function() {
   return gulp.src( 'source/sass/styles.scss' )
-    .pipe( sass().on( 'error', sass.logError ) )
-    .pipe( minifyCSS() )
+    .pipe( sass( { outputStyle: 'compressed' } ) )
     .pipe( gulp.dest( 'build' ) );
 });
+
+// JS.
+gulp.task( 'js', function() {
+  return browserify( { entries: [
+    'source/js/scripts.js'
+  ] } )
+    .transform( 'babelify', { presets: [ '@babel/preset-env' ] } )
+    .bundle()
+    .pipe( source( 'scripts.min.js' ) )
+    .pipe( buffer() )
+    .pipe( uglify() )
+    .pipe( gulp.dest( './build' ) );
+});
+
+
+gulp.task( 'default', gulp.series( 'scss', 'js' ) );
